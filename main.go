cc741e8a938ef5dea695f50346441229e213c6ff
Efrén Fuentes <efrenfuentes@gmail.com>
2018-02-18 17:14:10 -0400

    add dynamic and static file sample

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"time"
 )
 
 type helloResponse struct {
@@ -15,6 +16,8 @@ func main() {
 	port := 8080
 
 	http.HandleFunc("/", helloHandler)
+	http.HandleFunc("/time", timeHandler)
+	http.HandleFunc("/static", staticHandler)
 
 	log.Printf("Server starting on port %v\n", port)
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%v", port), nil))
@@ -25,4 +28,14 @@ func helloHandler(w http.ResponseWriter, r *http.Request) {
 
 	encoder := json.NewEncoder(w)
 	encoder.Encode(response)
+}
+
+func timeHandler(w http.ResponseWriter, r *http.Request) {
+	response := "The time is now " + time.Now().String()
+
+	fmt.Fprintf(w, response)
+}
+
+func staticHandler(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "static.html")
 }
